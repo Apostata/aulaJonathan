4d3cd7e32d0d983f6e3869cbf06756e051c0fee6
Jonathan <jonathandanilo79@gmail.com>
2024-09-28 10:49:01 -0300

    primeira aula do jonathan

---
+++ condicionais.js
@@ -0,0 +1,37 @@
+// condicionais
+// if (condição) {bloco de código} else {bloco de código} 
+
+// var numero = "1"
+
+// para comparar valores, utilizamos o operador de comparação == ou ===
+// == compara apenas o valor
+// === compara o valor e o tipo
+// no javascript, é recomendado utilizar o operador === para evitar erros, em outras linguagens, é recomendado utilizar o operador ==
+
+// if(numero === 1) {
+// 	console.log("O número é 1")
+// } else {
+// 	console.log("O número não é 1")
+// }
+
+// var num = 65
+
+// var resultado = num % 2     // 65 / 2 = 32 e sobra 1
+// resultado = num / 2 // divisão
+// resultado = num * 2 // multiplicação
+// resultado = num + 2 // soma
+// resultado = num - 2 //	subtração
+// resultado = num ** 2 // exponenciação
+
+// console.log(resultado)
+
+//  var name = "77"
+
+//  var resultado = name * 4
+//  console.log(resultado)
+ var name = 43
+ var resultado =name % 2
+ if(resultado === 0) {
+	console.log("o número é par ")
+ }
+else { console.log(" o número nao é par")}
---
+++ variaveis.js
@@ -0,0 +1,25 @@
+
+// Vaiaraveis são "caixas" que armazenam valores
+var numero = 1
+var nome = "jonathan"
+
+// nome da variável é o "endereço" da memória onde o valor está armazenado e armaena o valor da variável
+// ex: var nome = "jonathan" jonathan é o nome da variável, e "jonathan" é o valor da variável
+// string nome = "jonathan"  em linguagens tipadas, é necessário declarar o tipo da variável
+
+// int, ou integer, é um tipo de dado que representa números inteiros
+// float, ou double, é um tipo de dado que representa números decimais = 1.3
+// string é um tipo de dado que representa textos
+console.log(nome)
+
+nome = "Danilo"
+console.log(nome)
+
+nome = 1 // não é permitido em linguagens tipadas
+console.log(nome)
+
+
+
+
+
+
